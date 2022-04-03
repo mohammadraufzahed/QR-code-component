def0f1f49e80726233431acaa673e860b1fe0490
Mohamamd Raufzahed <mohamamdraufzahed@protonmail.com>
2022-04-03 22:19:12 +0430

    Added the QRCode Box Component

---
+++ website/src/app.jsx
@@ -0,0 +1,9 @@
+import QRBox from "./component/QRBox";
+
+export default function App(props) {
+  return (
+    <>
+      <QRBox />
+    </>
+  );
+}
---
+++ website/src/component/QRBox/index.jsx
@@ -0,0 +1,20 @@
+import classes from "../../styles/component/QRBox/index.module.scss";
+
+const QRBox = () => {
+  return (
+    <div className={classes.container}>
+      <figure className={classes.image}>
+        <img src="/image-qr-code.png" />
+      </figure>
+      <h3 className={classes.title}>
+        Improve your front-end skills by building projects
+      </h3>
+      <p className={classes.paragraph}>
+        Scan the QR code to visit Frontend Mentor and take your coding skills to
+        the next level
+      </p>
+    </div>
+  );
+};
+
+export default QRBox;
